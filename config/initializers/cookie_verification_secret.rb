b8cb34a8386a0d2aef82d6981800915e70cdb314
stainless <johnsonqu@gmail.com>
2010-12-27 21:10:48 +0800

    rails version

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '5f101c49cd65498832cbd5ca8c501144a1f10bede645f54385dddb73ce1a95789abb0c61d0bfb5aaf7ab7294f49bb84dc057346e20948552fd8dc7012137bc08';
+# ActionController::Base.cookie_verifier_secret = '5f101c49cd65498832cbd5ca8c501144a1f10bede645f54385dddb73ce1a95789abb0c61d0bfb5aaf7ab7294f49bb84dc057346e20948552fd8dc7012137bc08';
